2e38739dd68ffe04df200fab72bdfd8c9b754201
paw102 <paw1023021@gmail.com>
2025-03-06 21:42:14 +0900

    20250306 weather / github api uploaded

--- githubapiapp/src/App.tsx
+++ githubapiapp/src/App.tsx
@@ -1,10 +1,44 @@
+import axios from 'axios'
+import { useState } from 'react'
 import './App.css'
 
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+};
+
 function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repodata, setRepodata] = useState<Repository[]>([]);
+
+  const handleClick = () => {
+    axios.get<{items: Repository[]}>(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => setRepodata(response.data.items))
+    .catch(err => console.log(err))
+  }
 
   return (
     <>
-
+      <input value={keyword} onChange={e => setKeyword(e.target.value)} placeholder='web_development_react'/>
+      <br /><br />
+      <button onClick={handleClick}>Fetch</button>
+      {repodata.length === 0 ? (
+        <p>접근 가능한 데이터가 없습니다</p>
+      ):(
+        <table>
+          <tbody>
+            {repodata.map(repo => (
+              <tr key={repo.id}>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
