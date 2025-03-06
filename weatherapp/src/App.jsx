e4927e6187213a7051b98d6230abbeae568f7873
paw102 <paw1023021@gmail.com>
2025-03-06 20:34:20 +0900

    20250306 weather API

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,12 +1,37 @@
+import { useState ,useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [weather, setWeather] = useState({
+    temp: '',
+    desc: '',
+    icon: '',
+  });
 
-  return (
-    <>
-      
-    </>
-  )
+  useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=Metric&APIkey=dfe681564a7b190f03781f2a3cb641cc')
+    .then(Response => Response.json())
+    .then(result => {
+      setWeather({
+        temp: result.main.temp,
+        desc: result.weather[0].description,
+        icon: result.weather[0].icon,
+      });
+    })
+    .catch(err => console.log(err))
+  },[]);
+  
+  if(weather.icon) {
+    return (
+      <>
+        <p>온도 : {weather.temp} ℃</p>
+        <p>설명 : {weather.desc}</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="날씨 아이콘" />
+      </>
+    )
+  }
+  else {
+    return <div>로딩 중...</div>
+  }
 }
-
 export default App
