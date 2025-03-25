58570ab92a2e527ba22f6dd3b16b6e920cb18880
paw102 <paw1023021@gmail.com>
2025-03-25 21:27:35 +0900

    20250325 movieapp router

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,40 +1,23 @@
-import { useState, useEffect } from 'react'
-import Movie from './components/movie';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css'
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [ loading, setLoading ] = useState(true);
-  const [ movies, setMovies ] = useState([]);
-
-  // async - await
-  const getMovies = async() => {
-    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }   // -> 이걸 useEffect 에 넣어서 렌더링 될 때 한번만 getMovies 라는 함수를 실행시키겠다
-
-  useEffect(() => {
-    getMovies();
-  },[]);
-
-  // console.log(movies);   // 장르 받아올건데 장르가 배열로 되어있음
-
-  
   return (
-    <div>
-      {loading ? <h1>로딩 중...🎞</h1> : <div>{movies.map(movie => 
-        <Movie 
-          key={movie.id}
-          coverImg = {movie.medium_cover_image}
-          title = {movie.title}
-          summary = {movie.summary}
-          genres = {movie.genres}/>
-      )}</div>
-      }
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+    </Switch>
+    </Router>
+
+  );
 }
 
 export default App
--- movieapp/src/components/movie.jsx
+++ movieapp/src/components/movie.jsx
@@ -1,8 +1,12 @@
-function Movie({ key, coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
   return(
-    <div key={key}>
+    <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((genre) => (<li key={genre}>{genre}</li>))}
